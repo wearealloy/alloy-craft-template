4bc2edf11331eaea48baaa07ec2665b75aa3c9b5
Jorge Sosa <jsosa@wearealloy.com>
2019-01-29 10:31:54 -0600

    added waypoints package

--- dev/assets/_js/main.js
+++ dev/assets/_js/main.js
@@ -7,6 +7,7 @@ import Foundation from 'foundation-sites';
 
 // import JS files here using ES6 import statement
 import slick from 'slick-carousel';
+import waypoint from '../../../node_modules/waypoints/lib/noframework.waypoints';
 
 
 $(document).foundation()
