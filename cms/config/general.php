0799da7c08032f0a021a3ce3847d558f2706111d
Jorge Sosa <jsosa@wearealloy.com>
2019-05-29 18:24:41 -0500

    updated general config

--- cms/config/general.php
+++ cms/config/general.php
@@ -5,14 +5,17 @@
  * All of your system's general configuration settings go in here. You can see a
  * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
  *
- * @see \craft\config\GeneralConfig
+ * @see craft\config\GeneralConfig
  */
 
 return [
     // Global settings
     '*' => [
         // Default Week Start Day (0 = Sunday, 1 = Monday...)
-        'defaultWeekStartDay' => 1,
+        'defaultWeekStartDay' => 0,
+
+        // Enable CSRF Protection (recommended)
+        'enableCsrfProtection' => true,
 
         // Whether generated URLs should omit "index.php"
         'omitScriptNameInUrls' => true,
@@ -22,27 +25,39 @@ return [
 
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
-
-        // Whether to save the project config out to config/project.yaml
-        // (see https://docs.craftcms.com/v3/project-config.html)
-        'useProjectConfigFile' => false,
+        // custom aliases
+        'aliases' => [
+            '@assetBasePath' => getenv('ASSET_BASE_PATH'),
+            '@assetBaseUrl' => getenv('ASSET_BASE_URL'),
+            '@mediaBasePath' => getenv('MEDIA_BASE_PATH'),
+            '@mediaBaseUrl' => getenv('MEDIA_BASE_URL'),
+        ],
     ],
 
     // Dev environment settings
     'dev' => [
-        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
+        // Base site URL
+        'siteUrl' => '',
+
+        // Dev Mode (see https://craftcms.com/support/dev-mode)
         'devMode' => true,
     ],
 
     // Staging environment settings
+    'demo' => [
+        // Base site URL
+        'siteUrl' => '',
+    ],
+
+    // Staging environment settings
     'staging' => [
-        // Set this to `false` to prevent administrative changes from being made on staging
-        'allowAdminChanges' => true,
+        // Base site URL
+        'siteUrl' => '',
     ],
 
     // Production environment settings
     'production' => [
-        // Set this to `false` to prevent administrative changes from being made on production
-        'allowAdminChanges' => true,
+        // Base site URL
+        'siteUrl' => '',
     ],
 ];
